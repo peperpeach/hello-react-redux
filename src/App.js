5e94b6322934f1843b7fcd99209c963acb9d0704
peperpeach <peper_peach@hotmail.com>
2019-07-19 22:44:02 +0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { increment, decrement } from './actions';
+
+const App = ({ message, counter, dispatch }) => (
+  <div className="container">
+    <div className="columns column is-12">
+      <h1>Counter : { counter }</h1>
     </div>
-  );
+
+    <div className="buttons">
+      <button onClick={() => dispatch(increment(1))} className="button is-primary">
+        +1
+      </button>
+      <button onClick={() => dispatch(increment(2))} className="button is-link">
+        +2
+      </button>
+      <button onClick={() => dispatch(increment(3))} className="button is-info">
+        +3
+      </button>
+    </div>
+
+    <div className="buttons">
+      <button onClick={() => dispatch(decrement(1))} className="button is-primary">
+        -1
+      </button>
+      <button onClick={() => dispatch(decrement(2))} className="button is-link">
+        -2
+      </button>
+      <button onClick={() => dispatch(decrement(3))} className="button is-info">
+        -3
+      </button>
+    </div>
+  </div>
+)
+
+const mapStateToProps = state => {
+  return {
+    message: 'This is message from mapStateToProps',
+    counter: state.counter || 0,
+  }
 }
 
-export default App;
+const AppWithConnect = connect(mapStateToProps)(App)
+
+export default AppWithConnect;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker'
+import rootReducer from './reducers';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(logger));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const MyApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+ReactDOM.render(<MyApp />, document.getElementById('root'));
+registerServiceWorker();
